cf628e30d5bccadd18a30da4dace6a62d58bc4aa
Mikael Peña <mikyfuturistico@outlook.com>
2014-03-13 10:30:57 -0600

    commit Arboles

---
+++ src/arbol/Arbol.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package arbol;
+
+/**
+ *
+ * @author Alumnos
+ */
+public class Arbol <T>{
+    private Nodo<T> raiz;
+
+    public Nodo<T> getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(Nodo<T> raiz) {
+        this.raiz = raiz;
+    }
+    public void Recorrer(){
+        PilaArreglo <T> pila=new PilaArreglo<>();
+        Nodo <T> aux;
+        while(raiz!=null){
+            
+        }
+    }
+}
